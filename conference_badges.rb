2036e4b50477708d9662b600fe66708c783ab2f9
Patrick Hart <pathart612@gmail.com>
2022-05-16 11:03:12 -0400

    commit tree 6b957f9e05b3dc1d8e879da8f1a6a8f3e182fd93

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator name_array
+    name_array.map do |name| 
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms (speakers)
+    speakers.map.with_index(1) do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer names
+    batch_badge_creator(names).each do |nametag|
+        puts nametag
+    end
+    assign_rooms(names).each do |assignment|
+        puts assignment
+    end
+end
